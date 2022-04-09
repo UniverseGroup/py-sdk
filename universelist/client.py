28aa63706821ac089f9a5fdddceaf708d75e2fbc
InsanePhin <insanephin@gmail.com>
2022-04-09 12:27:51 +0900

    feat: uvlist client

---
+++ universelist/client.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+from aiohttp import ClientSession
+
+from .http import Router
+from .models import Bot, User, Vote
+
+
+class UniverxeListClient(Router):
+    def __init__(
+        self, token: Optional[str] = None, session: Optional[ClientSession] = None
+    ):
+        super().__init__(token, session)
+
+    async def bot(self, id: int) -> Bot:
+        return Bot(await self.get_bots(id))
+
+    async def user(self, id: int) -> User:
+        return User(await self.get_users(id))
+
+    async def is_voted_bot(self, bot_id: int, user_id: int) -> bool:
+        return Vote(await self.get_is_voted_bot(bot_id, user_id))
+
+    async def update_stat(self, id: int, count: int) -> bool:
+        return await self.post_bot_stat(id, count)
