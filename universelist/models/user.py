c0daae8a1b362b35034903e95775afcb9baa12bc
InsanePhin <insanephin@gmail.com>
2022-04-09 15:58:12 +0900

    chore: set default

--- universelist/models/user.py
+++ universelist/models/user.py
@@ -9,17 +9,17 @@ from .bot import Bot
 class User:
     id: int
     username: str
-    descriminator: int
+    descriminator: str
     avatar: str
-    bots: List[Bot] = []
-    badges: List[Badge] = []
+    bots: List[Bot]
+    badges: List[Badge]
 
     def __init__(self, data: dict) -> None:
         super().__init__(
-            id=data.get("id"),
-            username=data.get("username"),
-            descriminator=data.get("descriminator"),
-            avatar=data.get("avatar"),
-            bots=data.get("bots"),
-            badges=data.get("badges"),
+            id=data.get("id", 0),
+            username=data.get("username", ""),
+            descriminator=data.get("descriminator", ""),
+            avatar=data.get("avatar", ""),
+            bots=data.get("bots", []),
+            badges=data.get("badges", []),
         )
